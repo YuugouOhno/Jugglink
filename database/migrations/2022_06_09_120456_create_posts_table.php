f5922882eaf448c559761e9850fe919e32f9dfeb
YuugouOhno <yuugou.ohno@gmail.com>
2022-06-09 14:16:58 +0900

    migrationファイルを追加

---
+++ database/migrations/2022_06_09_120456_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('tool_id')->unsigned();
+            $table->integer('tool_number');
+            $table->string('technique', 20);
+            $table->string('video', 1);
+            $table->string('post_text', 100)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
