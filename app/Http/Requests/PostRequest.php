781d4beef6a6576fd684166a1d6aed2561dceebe
YuugouOhno <yuugou.ohno@gmail.com>
2022-07-22 20:23:52 +0900

    エラーの修正

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -9,11 +9,11 @@ class PostRequest extends FormRequest
     public function rules()
     {
         return [
-            'post.video' => 'required',
-            'post.technique' => 'required|string|max:50',
-            'post.tool_id' => 'required',
-            'post.tool_number' => 'required',
-            'post.text' => 'required|string|max:100'
+            // 'post.video' => 'required',
+            // 'post.technique' => 'required|string|max:50',
+            // 'post.tool_id' => 'required',
+            // 'post.tool_number' => 'required',
+            // 'post.text' => 'required|string|max:100'
         ];
     }
 }
